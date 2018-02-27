fd404ea33ea4233f3148bdde29b17d85bd7b3296
Malin Øien <malin.oien@gmail.com>
2018-02-27 11:11:05 +0100

    Import av log4j

---
+++ Gjesdalkommune/src/java/LifterClass.java
@@ -0,0 +1,18 @@
+package java;
+import org.apache.jena.rdf.model.InfModel;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+
+public class LifterClass {
+	/*
+	 * Denne klassen skal gå gjennom datasettene, og løfte de.
+	 */
+	public static void main(String args[]) {
+
+		Model rdfModel = ModelFactory.createDefaultModel();
+		InfModel rdfsModel = ModelFactory.createRDFSModel(rdfModel);
+
+		System.out.println("ehi");
+	}
+
+}
