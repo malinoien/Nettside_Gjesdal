6d9ce3a123aab0d904831a665a582f990af7d15c
Malin Øien <malin.oien@gmail.com>
2018-02-27 11:59:48 +0100

    Lese fra fil

--- Gjesdalkommune/src/prosjekt/LifterClass.java
+++ Gjesdalkommune/src/prosjekt/LifterClass.java
@@ -1,4 +1,9 @@
 package prosjekt;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
 import org.apache.jena.rdf.model.InfModel;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
@@ -11,8 +16,25 @@ public class LifterClass {
 
 		Model rdfModel = ModelFactory.createDefaultModel();
 		InfModel rdfsModel = ModelFactory.createRDFSModel(rdfModel);
-
-		System.out.println("ehi");
+		
+		
+		URL url;
+		try {
+			url = new URL("https://opencom.no/dataset/d1a3a50b-0566-48c1-acc0-15049da971b3/resource/fd6dc8bc-5df7-469f-81ec-9df61b67179e/download/grillplasser-i-gjesdal-kommune.csv");
+			try {
+				Scanner s = new Scanner(url.openStream());
+				while(s.hasNext()) {
+				System.out.println(s.next());
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
