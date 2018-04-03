b602874b7712c6e6ba27a1679af0adc0eacacd53
Malin Øien <malin.oien@gmail.com>
2018-04-03 10:26:48 +0200

    Skrive til fil

--- Gjesdalkommune/src/prosjekt/Model.java
+++ Gjesdalkommune/src/prosjekt/Model.java
@@ -1,6 +1,7 @@
 package prosjekt;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 import org.apache.jena.ontology.Individual;
 import org.apache.jena.ontology.OntClass;
@@ -8,6 +9,7 @@ import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.ontology.Ontology;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.util.iterator.ExtendedIterator;
 import org.omg.CORBA.portable.InputStream;
 
@@ -15,7 +17,8 @@ public class Model {
 
 	public static void main(String[] args) {
 		OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
-		String base = "..\\Gjesdalkommune\\src\\prosjekt\\onto.owl";
+		String base = "../Gjesdalkommune/src/prosjekt/onto.owl";
+		
 
 		try {
 			FileInputStream input = new FileInputStream(base);
@@ -25,12 +28,21 @@ public class Model {
 			System.out.println(e.getMessage());
 		}
 
-		ExtendedIterator<Individual> itI = model.listIndividuals();
-		ExtendedIterator<OntClass> itL = model.listClasses();
-		while (itL.hasNext()) {
-			OntClass i = itL.next();
-			System.out.println(i.getLocalName());
-		}
+		ExtendedIterator<Individual> itInd = model.listIndividuals();
+		ExtendedIterator<OntClass> itCla = model.listClasses();
+		while (itCla.hasNext()) {
+			OntClass i = itCla.next();
+			//if(i.hasSuperClass()) System.out.println(i.getSuperClass());
 
+		}
+		System.out.println();
+		
+		model.write(System.out, "TURTLE");
+		
+		try {
+            model.write(new FileOutputStream("Gjesdal.ttl"), "TURTLE");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
 	}
 }
