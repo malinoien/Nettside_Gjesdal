4dca3fcc10ac851f118dffcf1df31773c52e8286
Malin Øien <malin.oien@gmail.com>
2018-04-23 12:36:59 +0200

    Fikset litt på sio

--- Nettside/script.js
+++ Nettside/script.js
@@ -1,5 +1,6 @@
 window.onload = function(){
 document.getElementById("søkeknapp").addEventListener("click", sok);
+document.getElementById("fjernChecked").addEventListener("click", fjernValgteSjekkbokser);
 console.log("onload");
 }
 
@@ -23,10 +24,23 @@ function sok(){
   if(document.getElementById("utleielokale").checked) spørring += " {?subject a mr:RentalProperty}";
   if(document.getElementById("skole").checked) spørring += " {?subject a mr:School}";
 
-  
+
 
   Q.query(prefix + navnlatlon + spørring + slutt)
           .endpointURL("http://localhost:3030/Gjesdal/query")
           .chartFunction("sgvizler.visualization.Map")
-          .draw("example");
+          .draw("map");
+  }
+
+  function fjernValgteSjekkbokser(){
+    document.getElementById("skjenkested").checked = false;
+    document.getElementById("overnatting").checked = false;
+    document.getElementById("grillplass").checked = false;
+    document.getElementById("badeplass").checked = false;
+    document.getElementById("kirke").checked = false;
+    document.getElementById("fiskeplass").checked = false;
+    document.getElementById("barnehage").checked = false;
+    document.getElementById("avfallspunkt").checked = false;
+    document.getElementById("utleielokale").checked = false;
+    document.getElementById("skole").checked = false;
   }
